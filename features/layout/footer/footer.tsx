5d0dc9d3aca22f2f15b8a9b361041c664d9dffb4
Felice Forgione <forgione67@gmail.com>
2024-07-06 15:13:17 -0400

    Use space variables in footer css

--- features/layout/footer/footer.tsx
+++ features/layout/footer/footer.tsx
@@ -8,10 +8,10 @@ export function Footer() {
       <div className={styles.container}>
         <div className={styles.content}>
           <div className={styles.footerLinks}>
-            <Link href="#">Docs</Link>
-            <Link href="#">API</Link>
-            <Link href="#">Help</Link>
-            <Link href="#">Community</Link>
+            <Link href="/#">Docs</Link>
+            <Link href="/#">API</Link>
+            <Link href="/#">Help</Link>
+            <Link href="/#">Community</Link>
           </div>
           <div className={styles.footerLogo}>
             {/* eslint-disable-next-line @next/next/no-img-element */}

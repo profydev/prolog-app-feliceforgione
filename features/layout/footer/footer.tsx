7cafced5b21fd2b71abc84b265578c1b850a13b3
Felice Forgione <93881327+feliceforgione@users.noreply.github.com>
2024-07-07 13:37:55 -0400

    Footer (#11)
    * Add footer starter files
    
    * Apply css to footer
    
    * Remove console.log from sidebar navigation
    
    * Use space variables in footer css
    
    * Add cypress test for footer
    
    * Fix cypress test to use correct nested selector syntax
    
    * Use server env to handle version number
    
    * Convert layout to use grid
    
    * Clean up footer cypress test

--- features/layout/footer/footer.tsx
+++ features/layout/footer/footer.tsx
@@ -1,23 +1,32 @@
 import Link from "next/link";
 import styles from "./footer.module.scss";
-import { version } from "../../../package.json";
 
 export function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
         <div className={styles.content}>
-          <div className={styles.footerLinks}>
-            <Link href="/#">Docs</Link>
-            <Link href="/#">API</Link>
-            <Link href="/#">Help</Link>
-            <Link href="/#">Community</Link>
-          </div>
+          <ul className={styles.footerLinks}>
+            <li>
+              <Link href="/#">Docs</Link>
+            </li>
+            <li>
+              <Link href="/#">API</Link>
+            </li>
+            <li>
+              <Link href="/#">Help</Link>
+            </li>
+            <li>
+              <Link href="/#">Community</Link>
+            </li>
+          </ul>
           <div className={styles.footerLogo}>
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img src="/icons/logo-small.svg" alt="logo" />
           </div>
-          <div className={styles.footerVersion}>Version: {version}</div>
+          <div className={styles.footerVersion}>
+            Version: {process.env.AppVersion}
+          </div>
         </div>
       </div>
     </footer>

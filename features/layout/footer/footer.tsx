d97e4a87a512595f36d6d5a82b67f4089f4ba4d6
Felice Forgione <forgione67@gmail.com>
2024-07-07 13:16:24 -0400

    Convert layout to use grid

--- features/layout/footer/footer.tsx
+++ features/layout/footer/footer.tsx
@@ -6,12 +6,20 @@ export function Footer() {
     <footer className={styles.footer}>
       <div className={styles.container}>
         <div className={styles.content}>
-          <div className={styles.footerLinks}>
-            <Link href="/#">Docs</Link>
-            <Link href="/#">API</Link>
-            <Link href="/#">Help</Link>
-            <Link href="/#">Community</Link>
-          </div>
+          <ul className={styles.footerLinks}>
+            <li>
+              <Link href="/#">Docs</Link>
+            </li>
+            <li>
+              <Link href="/#">API</Link>
+            </li>
+            <li>
+              <Link href="/#">Help</Link>
+            </li>
+            <li>
+              <Link href="/#">Community</Link>
+            </li>
+          </ul>
           <div className={styles.footerLogo}>
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img src="/icons/logo-small.svg" alt="logo" />
